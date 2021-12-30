70fe3cd16ce40292fb9740a3871b24f2eaf0600f
ekasunandika <ekasunandikaa@gmail.com>
2021-12-30 18:38:25 +0700

    add order.js

---
+++ controllers/order.js
@@ -0,0 +1,44 @@
+const { doc, collection, addDoc, getDocs, getDoc } = require("firebase/firestore");
+const db = require("../config");
+
+const createOrder = async (req, res) => {
+    try {
+        const data = req.body;
+        const docRef = await addDoc(collection(db, "orders"), data);
+        res.send({message: `New Order Added ${docRef.id}`});
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+
+const getOrder = async (req, res) => {
+    try {
+        const docRef = collection(db, "orders");
+        const docSnap = await getDocs(docRef);
+        const list = docSnap.docs.map((doc) => {
+            return { id: doc.id, ...doc.data() }
+        });
+        console.log(list);
+        res.json(list);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+
+const getOrderById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        console.log(id)
+        const docRef = doc(db, "orders", id);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            console.log(docSnap.data());
+            res.send(docSnap.data());
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message}) 
+    }
+}
+
+module.exports = { createOrder, getOrder, getOrderById };
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const { createUser, userValidate } = require("../controllers/user");
 const { getPackage, getPackageById } = require("../controllers/package");
+const { createOrder, getOrder, getOrderById } = require("../controllers/order");
 const router = express.Router();
 
 router.get("/package", getPackage);
 router.get("/package/:id", getPackageById);
-// router.get("/order", getOrder);
-// router.get("/order/:id", getOrderById);
+router.get("/order", getOrder);
+router.get("/order/:id", getOrderById);
 router.post("/user/validate", userValidate);
 router.post("/user", createUser);
-// router.post("/order", createOrder);
+router.post("/order", createOrder);
 // router.post("/order/:id", deleteOrder);
 
 
